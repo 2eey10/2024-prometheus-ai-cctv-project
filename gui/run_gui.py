f9edc820a8e0d5f8991897f0baacaf5307646e49
2eey10 <coherentman0103@naver.com>
2024-09-25 22:04:01 +0900

    feature: streamlit video inference 구현

--- gui/run_gui.py
+++ gui/run_gui.py
@@ -1,1 +1,19 @@
-# streamliit run run_gui.py
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+
+st.title("😄 5팀의 멀티모달 영상분석🚀")
+"""
+streamlit run run_gui.py
+"""
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    # OpenCV를 활용한 영상 처리 가능.
+    # 그레이스케일로 변환 시:
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
